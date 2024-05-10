79a5069875f21c40d5f7ac3a5a3bbc98396ae574
Brady Dean <bradyddean@gmail.com>
2024-05-10 03:02:22 +0000

    rendering helper

--- components/index_templ.go
+++ components/index_templ.go
@@ -60,7 +60,7 @@ func Clicked() templ.Component {
 			templ_7745c5c3_Var3 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<h1>You clicked the button!</h1>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<h2>You clicked the button!</h2>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
@@ -11,16 +12,54 @@ import (
 
 //go:generate go run github.com/a-h/templ/cmd/templ generate
 
+type RenderOption func(*RenderOptions)
+
+type RenderOptions struct {
+	Status      int
+	ContentType string
+}
+
+func WithStatus(status int) RenderOption {
+	return func(opt *RenderOptions) {
+		opt.Status = status
+	}
+}
+
+func WithContentType(contentType string) RenderOption {
+	return func(opt *RenderOptions) {
+		opt.ContentType = contentType
+	}
+}
+
+func Render(c echo.Context, component templ.Component, options ...RenderOption) error {
+	var opts RenderOptions
+
+	for _, opt := range options {
+		opt(&opts)
+	}
+
+	if opts.ContentType != "" {
+		c.Response().Header().Set(echo.HeaderContentType, opts.ContentType)
+	}
+
+	if opts.Status != 0 {
+		c.Response().Status = opts.Status
+	}
+
+	return component.Render(c.Request().Context(), c.Response())
+}
+
 func main() {
 	e := echo.New()
+
 	e.Group("/static").Use(middleware.Static("static"))
 
 	e.GET("/", func(c echo.Context) error {
-		return components.Index("World").Render(c.Request().Context(), c.Response().Writer)
+		return Render(c, components.Index("World"))
 	})
 
 	e.POST("/clicked", func(c echo.Context) error {
-		return components.Clicked().Render(c.Request().Context(), c.Response().Writer)	
+		return Render(c, components.Clicked(), WithStatus(http.StatusCreated))
 	})
 
 	if err := e.Start(":8000"); err != nil && err != http.ErrServerClosed {
