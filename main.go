a77eaf2be3a87eb93091bfa1c2a288677ca01f10
Brady Dean <bradyddean@gmail.com>
2024-05-13 03:32:17 +0000

    logging

--- main.go
+++ main.go
@@ -93,6 +93,6 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	if err := e.Shutdown(ctx); err != nil {
-		e.Logger.Fatal(err)
+		e.Logger.Fatal(fmt.Errorf("error shutting down the server: %w", err))
 	}
 }
